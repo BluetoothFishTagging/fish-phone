013c20e267a992a3655abebcd2574c921c9a1533
ksoltan <kattsoltan@gmail.com>
2016-02-26 16:04:09 -0500

    Added in the GPS module. Tested using a button that calls GPS activity from mainactivity. Need work on error testing

--- app/src/main/java/bft/fishtagsapp/MainActivity.java
+++ app/src/main/java/bft/fishtagsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package bft.fishtagsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +50,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getGPS(View view){
+        // Do something in response to button
+        Intent intent = new Intent(this, GPSActivity.class);
+        startActivity(intent);
     }
 }
